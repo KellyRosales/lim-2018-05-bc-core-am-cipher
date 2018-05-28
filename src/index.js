a57eeabd849fdbc8907d2eef81b69e61403fc6ab
Kelly <kellygrc20@gmail.com>
2018-05-28 16:13:33 -0500

    añadiendo cambios index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { encode } from "punycode";
+
+const textEncode= document.getElementById('text-encode');
+const buttonCipher =  document.getElementById('button-cipher');
+button-cipher.addEventListener('click', ()=>{});
+
+
+const textDecode= document.getElementById('text-decode');
+const buttonDecipher = document.getElementById('button-decipher');
+button-decipher.addEventListener('Click',()=>{});

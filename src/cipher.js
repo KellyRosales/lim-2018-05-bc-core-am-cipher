a95cea7734daf611d29803aa22c07a2c4efaa603
Kelly <kellygrc20@gmail.com>
2018-05-29 12:57:03 -0500

    Añadiendo cambios en el Js

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,41 @@
 window.cipher = {
-  // ... 
+  
+  encode: (offset,string) => {
+    //para llamar al offset y stringx
+    
+      var cipherText= ""; 
+      //variable que contenga el mensaje del texto ingresado
+
+      for ( i=0, i<string.length, i++) {
+      //Añadiendo la función for para la lógica del código ASCII de las letras
+
+        const chartText= (string.ChartCode(i)-65+33)%26+65;
+        //USando la fórmula del código ASCII para obtener el número deseado
+
+        const ChartTextTwo=string.fromCharCode (ChartText);
+        //Devolviendo el valor encontrado en letras
+    }
+    return cipherText;
+    console.log(ChartTextTwo);   
+   }
+
+          
+  decode: (offset,String) => {
+      var decipherText="";
+      //variable que contenga el mensaje de texto ingresado
+      for (i=0, i<string.length, i++) {
+        const 
+
+
+
+      }
+    cipher.decode(offset,string)
+    //USo el cipher.encode(offset,String) para hacer el llamado del DOM//
+  }
 };
- //agregando funciones
+
+
+
+return{
+  encode: (string) => {}
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import { encode } from "punycode";
 
 const textEncode= document.getElementById('text-encode');
 const buttonCipher =  document.getElementById('button-cipher');
-button-cipher.addEventListener('click', ()=>{});
+button-cipher.addEventListener('click', cipher.encode()=>{});
 
 
 const textDecode= document.getElementById('text-decode');
 const buttonDecipher = document.getElementById('button-decipher');
-button-decipher.addEventListener('Click',()=>{});
+button-decipher.addEventListener('Click', cipher.decode()=>{});

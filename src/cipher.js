1b6e4944bf842ab52628447ba352c424c0f0ef8a
Kelly <kellygrc20@gmail.com>
2018-06-04 03:04:53 -0500

    Proyecto cifrado cesar, versión presentación

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,28 @@
 window.cipher ={
-  encode: (offset,string) =>{
+  encode: (offset,string) => {
 
     let complete= '';
-    
-    for (let i=0; i<string.length ; i++){
-      let position= parseInt(offset);
-      let sentence=string.charCodeAt(i); 
-        //usamos charCodeAt para obtener el codigo ASCII de las letras insertads y fromChartcode para convertir el codigo en letras
 
-      if(sentence>=65 && sentence<=90) {      
-        complete=complete + String.fromCharCode((sentence - 65 + position)%26 + 65);
+    if(offset>0){
+      code = parseInt(offset);
+    } else {
+      code = alert('ingrese solo números enteros positivos');
+    }  // previniendo que solo se pueda insertar números en la clave del cifrado
+
+    for (let i=0; i<string.length ; i++){
+      let word=string.charCodeAt(i); 
+        //usamos charCodeAt para obtener el codigo ASCII de las letras insertads y fromChartcode para convertir el codigo en letras
+      if(word>=65 && word<=90) {      
+        complete = complete + String.fromCharCode((word - 65 + offset)%26 + 65);
         //para letras en mayúsculas
-      } else if(sentence>=97 && sentence<=122) {
-        complete=complete + String.fromCharCode((sentence - 97 + position)%26 + 97);
+      } else if(word>=97 && word<=122) {
+        complete = complete + String.fromCharCode((word - 97 + offset)%26 + 97);
         //para letras en minúsculas
-      } else if(sentence== 32) {
+      } else if(word== 32) {
         complete += ' ';
         //opción de insertar un espacio 
       } else {
-        complete=alert('Ingrese solo letras');
+        complete = alert('Ingrese solo letras');
         //previniendo errores
       }
     }
@@ -28,22 +32,26 @@ window.cipher ={
     
   decode:(offset,string) => {
     let completeD='';
+    if(offset>0) {
+      codeD = parseInt(offset);
+    } else {
+      codeD = alert('ingrese solo números enteros positivos');
+    }  // previniendo que solo se pueda insertar números en la clave del cifrado
   
     for (var i=0; i<string.length ; i++) {
-      let positionD=parseInt(offset);
-      let sentenceD=string.charCodeAt(i);
-
-      if(sentenceD>=65 && sentenceD<=90) {      
-        completeD=completeD+String.fromCharCode(((sentenceD-65)+(26-(positionD%26)))%26+65);  
+      let wordD=string.charCodeAt(i);
+        //usamos charCodeAt para obtener el codigo ASCII de las letras insertads y fromChartcode para convertir el codigo en letras
+      if(wordD>=65 && wordD<=90) {      
+        completeD = completeD+String.fromCharCode(((wordD-65)+(26-(offset%26)))%26+65);  
         //para letras en mayúsculas
-      } else if(sentenceD>=97 && sentenceD<=122) {
-        completeD=completeD+String.fromCharCode(((sentenceD-97)+(26-(positionD%26)))%26+97);
+      } else if(wordD>=97 && wordD<=122) {
+        completeD = completeD+String.fromCharCode(((wordD-97)+(26-(offset%26)))%26+97);
         //para letras en minúsculas
-      } else if(sentenceD== 32) {
+      } else if(wordD== 32) {
         completeD += ' ';
         //opción de insertar un espacio 
       } else {
-        completeD=alert('Ingrese solo letras');
+        completeD = alert('Ingrese solo letras');
         //previniendo errores
       }
     }
